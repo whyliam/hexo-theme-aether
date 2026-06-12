d650833027a66abb19229d6be0ce634ab5066840
行权 <chengzhang.lcz@alibaba-inc.com>
2026-06-12 23:25:32 +0800

    feat(comments): 添加 Waline 评论系统支持

---
+++ scripts/filters/comment/waline.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = hexo => {
+  const config = hexo.theme.config;
+  if (!config.waline || !config.waline.enable) return;
+
+  // Register comment filter
+  hexo.extend.filter.register('theme_inject', injects => {
+    injects.comment.raw('waline', '<div id="waline"></div>', {}, {cache: true});
+    
+    injects.bodyEnd.raw('waline', `
+      <link rel="stylesheet" href="https://unpkg.com/@waline/client@v3/dist/waline.css" />
+      <script type="module">
+        import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
+        init({
+          el: '#waline',
+          serverURL: '${config.waline.serverURL}',
+          ${config.waline.locale ? `locale: ${JSON.stringify(config.waline.locale)},` : ''}
+          ${config.waline.dark ? `dark: '${config.waline.dark}',` : ''}
+          ${config.waline.meta ? `meta: ${JSON.stringify(config.waline.meta)},` : ''}
+          ${config.waline.requiredMeta ? `requiredMeta: ${JSON.stringify(config.waline.requiredMeta)},` : ''}
+          ${config.waline.login ? `login: '${config.waline.login}',` : ''}
+          ${config.waline.wordLimit ? `wordLimit: ${config.waline.wordLimit},` : ''}
+          ${config.waline.pageSize ? `pageSize: ${config.waline.pageSize},` : ''}
+          ${config.waline.imageUploader !== undefined ? `imageUploader: ${config.waline.imageUploader},` : ''}
+          ${config.waline.highlighter !== undefined ? `highlighter: ${config.waline.highlighter},` : ''}
+          ${config.waline.texRenderer !== undefined ? `texRenderer: ${config.waline.texRenderer},` : ''}
+          ${config.waline.search !== undefined ? `search: ${config.waline.search},` : ''}
+          ${config.waline.reaction ? `reaction: ${JSON.stringify(config.waline.reaction)},` : ''}
+          path: window.location.pathname,
+        });
+      </script>
+    `, {}, {cache: true});
+  });
+};
